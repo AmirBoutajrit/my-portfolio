db66551a9fec6df870ec5466d981c18b6f506c6d
Amir Boutajrit <144557991+AmirBoutajrit@users.noreply.github.com>
2025-10-13 23:15:28 -0700

    code changes Code Changes

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+      <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+        <h2>My Portfolio</h2>
+      </nav>
+    )
+  }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+        <h2>My Portfolio</h2>
+      </nav>
+    )
+  }
